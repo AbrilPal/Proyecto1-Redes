bf6685a59f4c9a7796bea677d2db804c2cdfbde3
Abril Palencia <pal18198@uvg.edu.gt>
2021-08-08 21:54:43 -0600

    ClientXMPP

--- Cliente.py
+++ Cliente.py
@@ -1,3 +1,13 @@
 # Project No.1
 # Author: Abril Palencia
 # date: 07/08/2021
+
+# Libraries import
+import logging
+from getpass import getpass
+from argparse import ArgumentParser
+import slixmpp as xmpp
+
+class Cliente(xmpp.ClientXMPP):
+    def __init__(self, jid, password):
+        xmpp.ClientXMPP.__init__(self, jid, password)
