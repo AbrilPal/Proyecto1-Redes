904b77ac24dbc1027b727966dfa2a61317ea827d
Abril Palencia <pal18198@uvg.edu.gt>
2021-08-10 22:15:48 -0600

    reference

--- Cliente.py
+++ Cliente.py
@@ -1,6 +1,7 @@
 # Project No.1
 # Author: Abril Palencia
 # date: 07/08/2021
+# bilographic reference: https://slixmpp.readthedocs.io/en/latest/getting_started/index.html
 
 # Libraries import
 import logging
